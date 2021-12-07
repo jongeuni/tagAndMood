17946ffec9c7e31c0bf0475141394cdeb9abdc60
joungeun <lje0818ovo@naver.com>
2021-12-07 16:53:52 +0900

    [ADD] Login logic

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-# This is a sample Python script.
+import os.path
+
+from selenium import webdriver
+import pandas as pd
+
+from crawling.crawling import *
+from env import env_setting
+
+
+def crawling():
+    word = "슬픔"
+    url = insta_searching(word)
+
+    driver.get(url)
+    time.sleep(3)
+
+    select_first(driver)
+
+    results = []
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -9,18 +27,33 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-    from selenium import webdriver
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    import time
-    import re
+if __name__ == '__main__':
+
     driver = webdriver.Chrome()
     driver.implicitly_wait(2)
-    driver.get('https://www.daum.net/')
 
+    word = '슬픔'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # url = insta_searching(word)
+    # driver.get(url)
+    # time.sleep(4)
+
+    driver.get('https://www.instagram.com/accounts/login/')
+    time.sleep(2)
+
+    env_setting()
+
+    email = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+
+    input_id = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[0]
+    input_id.clear()
+    input_id.send_keys(email)
+
+    input_pw = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[1]
+    input_pw.clear()
+    input_pw.send_keys(password)
+    input_pw.submit()
+
+    #crawling()
