c990dacb7f3975fadf80d3176d765eee0e2573de
joungeun <lje0818ovo@naver.com>
2021-12-09 15:25:50 +0900

    [REFACTOR] Automate the part where you read CSV files
    그래프로 변환할 때 다양한 파일을 사용하기 때문에 나중을 위해 자동화했습니다. csv 파일을 바꿀 때마다 코드를 바꿀 수는 없으니까요.

--- visualization/main.py
+++ visualization/main.py
@@ -2,15 +2,18 @@ import emoji
 import pandas as pd
 import re
 
+from pandas import read_csv
+from visualization.graph import create_graph
 
-def emoji_count(c):  # 이모지 갯수 새기!
+
+def emoji_count(c):
     emoji_count_lis = []
     for i in c:
         emoji_count_lis.append(emoji.emoji_count(i))
     return emoji_count_lis
 
 
-def letter_count(c):  # 글자 수 새기!
+def letter_count(c):
     letter_count_lis = []
 
     for i in c:
@@ -19,7 +22,7 @@ def letter_count(c):  # 글자 수 새기!
     return letter_count_lis
 
 
-def tag_count(c):  # 태그 수 새기!
+def tag_count(c):
     tag_count_lis = []
     for i in c:
         tags = re.findall(r'#[^\s#,\\]+', i)
@@ -28,15 +31,20 @@ def tag_count(c):  # 태그 수 새기!
 
 
 if __name__ == '__main__':
-    # row = ['content', 'date', 'tag_count', 'letter_count', 'imoji_count']
-    df = pd.read_csv('../csv/happy_행복한하루.csv', header='infer', encoding='utf-8')  # DataFrame
-    content = df['content'].tolist()
-    fi = pd.DataFrame({
-        'content' : df['content'].tolist(),
-        'date' : df['data'].tolist(),
-        'tag_count':tag_count(content),
-        'letter_count':letter_count(content),
-        'emoji_count':emoji_count(content)
-    })
-    print(fi)
+    num = int(input('몇 개의 csv 파일을 가져올까요?'))
+    csv_files = []
+    dfs = []
+    for i in range(0, num):
+        file_name = input('가져올 csv 파일의 이름을 말해주세요')
+        csv_files.append(read_csv('../csv/' + file_name + '.csv', header='infer', encoding='utf-8'))
+        content = csv_files[i]['content'].tolist()
 
+        dfs.append(pd.DataFrame({
+            'content': csv_files[i]['content'].tolist(),
+            'date': csv_files[i]['data'].tolist(),
+            'tag_count': tag_count(content),
+            'letter_count': letter_count(content),
+            'emoji_count': emoji_count(content)
+        }))
+
+    create_graph(dfs[0])
