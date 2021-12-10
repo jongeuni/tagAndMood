3767438e0cea07f422e43cfbeef8274f6dfd22c3
joungeun <lje0818ovo@naver.com>
2021-12-10 13:03:12 +0900

    [ADD] count.py

---
+++ visualization/count.py
@@ -0,0 +1,26 @@
+import re
+import emoji
+
+
+def emoji_count(c):
+    emoji_count_lis = []
+    for i in c:
+        emoji_count_lis.append(emoji.emoji_count(i))
+    return emoji_count_lis
+
+
+def letter_count(c):
+    letter_count_lis = []
+
+    for i in c:
+        letter_count_lis.append(len(i))
+
+    return letter_count_lis
+
+
+def tag_count(c):
+    tag_count_lis = []
+    for i in c:
+        tags = re.findall(r'#[^\s#,\\]+', i)
+        tag_count_lis.append(len(tags))
+    return tag_count_lis
