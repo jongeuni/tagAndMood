9f86b48172aed9df709e1b5090e2ef62a2555c40
joungeun <lje0818ovo@naver.com>
2021-12-11 22:24:47 +0900

    [ADD] Emoji graph def

--- visualization/graph.py
+++ visualization/graph.py
@@ -53,3 +53,26 @@ def avg_comparison_graph(sad, happy):
 
     plt.legend()
     plt.show()
+
+
+def all_emoji_graph(happy_one, happy_tow, happy_three,
+                        sad_one, sad_tow, sad_three):  # 이모지 그래프
+    hangle()
+    plt.title('이모티콘 사용 빈도 비교')
+    plt.scatter(range(100), happy_one, color='pink')
+    plt.scatter(range(100), happy_tow, color='pink')
+    plt.scatter(range(100), happy_three, color='pink')
+    plt.scatter(range(100), sad_one, color='aquamarine')
+    plt.scatter(range(100), sad_tow, color='aquamarine')
+    plt.scatter(range(100), sad_three, color='aquamarine')
+    plt.show()  # 산점도
+
+    plt.hist(x= range(100), weights=happy_one, bins = 50, label = '#행복해요', color = 'pink', alpha=0.5)
+    plt.hist(x=range(100), weights=happy_tow, bins=50, label='#행복햄', color='violet', alpha=0.5)
+    plt.hist(x=range(100), weights=happy_three, bins=50, label='#행복한하루', color='palevioletred', alpha=0.5)
+    plt.hist(x=range(100), weights=sad_one, bins=50, label='#슬프다', color='aquamarine', alpha=0.5)
+    plt.hist(x=range(100), weights=sad_tow, bins=50, label='#우울해', color='skyblue', alpha=0.5)
+    plt.hist(x=range(100), weights=sad_three, bins=50, label='#슬픈하루', color='lightsteelblue', alpha=0.5)
+    plt.title('이모티콘 사용 빈도 비교')
+    plt.legend(title='tag')
+    plt.show()
